a3472d8203752faf961ea2e56dc978466330f87f
Mahsa Jalili <jalili.mah@gmail.com>
2026-09-25 13:35:28 +0200

    updated

--- R/07_null_models.R
+++ R/07_null_models.R
@@ -32,5 +32,41 @@ m0_svylme <- svy2lme(kcal ~ 1 + (1 | SEQN), design = des_person,
 set.seed(2017)
 des_psu <- svydesign(ids = ~SDMVPSU, strata = ~SDMVSTRA,
                      weights = ~WTDR2D, nest = TRUE, data = long_adults)
-rep_des <- as.svrepdesign(des_psu, type = "subbootstrap", replicates = 100)
+rep_des <- as.svrepdesign(des_psu, type = "subbootstrap", replicates = 500)
 m0_boot <- boot2lme(m0_svylme, rep_des)
+vc <- function(m) as.data.frame(VarCorr(m))$sdcor
+
+lmer_row <- function(m, name) {
+  tibble(estimator = name,
+         intercept = fixef(m)[[1]],
+         SE        = sqrt(vcov(m)[1, 1]),
+         sd_between = vc(m)[1],
+         sd_within  = vc(m)[2])
+}
+
+sv_between <- as.numeric(sqrt(m0_svylme$s2 * as.vector(m0_svylme$L)[1]))
+sv_within  <- as.numeric(sqrt(m0_svylme$s2))
+
+results_null <- bind_rows(
+  lmer_row(m0,    "lmer"),
+  lmer_row(m0_w,  "lmer, raw weights"),
+  lmer_row(m0_wn, "lmer, rescaled weights"),
+  tibble(estimator  = "WeMix",
+         intercept  = m0_wemix$coef[[1]],
+         SE         = m0_wemix$SE[[1]],
+         sd_between = sqrt(m0_wemix$vars[[1]]),
+         sd_within  = sqrt(m0_wemix$vars[[2]])),
+  tibble(estimator  = "svylme",
+         intercept  = coef(m0_svylme)[[1]],
+         SE         = sqrt(vcov(m0_svylme)[1, 1]),
+         sd_between = sv_between,
+         sd_within  = sv_within),
+  tibble(estimator  = "svylme + PSU bootstrap",
+         intercept  = coef(m0_svylme)[[1]],
+         SE         = sqrt(vcov(m0_boot)[1, 1]),
+         sd_between = sv_between,
+         sd_within  = sv_within)
+) %>%
+  mutate(ICC = sd_between^2 / (sd_between^2 + sd_within^2))
+
+results_null
